3e98e09ef8f1e6a77b96e86aa576cb76fa59e9a6
louisewebfx <90263713+louisewebfx@users.noreply.github.com>
2021-09-08 21:37:43 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,11 @@
 
     $('.webform-submission-swim-lesson-selector-form').on( 'input', e => {
         const form = e.currentTarget
-        console.log(form)
         if( 'undefined' !== MCFX ) {
             MCFX.Form.submit( form )
             console.log('form-submmited')
         }
     })
-     $('.webform-submission-swim-lesson-selector-form').on( 'input', e => {
-            console.log(e.currentTarget);
-      });
+    
 
 }) ( jQuery )
