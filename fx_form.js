b575215a02d808c367921a765c9c3a98da4ff158
louisewebfx <90263713+louisewebfx@users.noreply.github.com>
2021-09-17 21:00:43 +0800

    Create fx_form.js

---
+++ fx_form.js
@@ -0,0 +1,12 @@
+$(function(){
+  $('.ant-btn').on('click',function(){ 
+   console.log('appending step1 form')
+  var email = $('[name=email]').val();
+  var firstname = $('[name=first_name]').val();
+  var lastname = $('[name=last_name]').val();
+$('.ant-form').append('<input type="hidden" name="email" value="' + email+ '">');
+  $('.ant-form').append('<input type="hidden" name="first_name" value="' + firstname+ '">');
+  $('.ant-form').append('<input type="hidden" name="last_name" value="' + lastname+ '">');
+});
+
+});
