36ecf331605f89ac74d10e004c280e9ff8c44b3a
louisewebfx <90263713+louisewebfx@users.noreply.github.com>
2021-09-18 01:52:32 +0800

    Create fxform.js

---
+++ fxform.js
@@ -0,0 +1,19 @@
+$(document).on("click", ".step1 button.ant-btn-secondary" , function() {
+     var ga = $(this).closest('.step1').find('[name=ga_id]').val();
+     var email = $(this).closest('.step1').find('[name=email]').val();
+     var firstname = $(this).closest('.step1').find('[name=first_name]').val(); 
+     var lastname = $(this).closest('.step1').find('[name=last_name]').val(); 
+     $('.ant-form').append('<input type="hidden" name="email" value="' + email+ '">');
+     $('.ant-form').append('<input type="hidden" name="first_name" value="' + firstname+ '">');
+     $('.ant-form').append('<input type="hidden" name="last_name" value="' + lastname+ '">');
+      if(ga === undefined) {
+          $('.ant-form').append('<input type="hidden" name="mcfx_form_id" value="61428b8bf4e4f1909c6d1ac7">');
+          $('.ant-form').append('<input type="hidden" name="fx_type" value="Sign Up">');
+          $('.ant-form').append('<input type="hidden" name="ga_id" value="' + localStorage.getItem("ga_id")+ '">');
+          $('.ant-form').append('<input type="hidden" name="ga_count" value="' + localStorage.getItem("ga_count")+ '">');
+          $('.ant-form').append('<input type="hidden" name="lmfx_submit_uuid" value="' + localStorage.getItem("lmfx_submit_uuid")+ '">');
+    
+        
+      }
+    
+ });
