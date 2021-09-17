3e69ca5cac914a8bd8149f4abb5770e69d4cc223
louisewebfx <90263713+louisewebfx@users.noreply.github.com>
2021-09-18 01:48:34 +0800

    Create fxsubmitform.js

---
+++ fxsubmitform.js
@@ -0,0 +1,9 @@
+
+$(document).on("click", ".step2 button.ant-btn-secondary" , function() {
+         
+         var $form = $('#root form');
+         MCFX.Form.submit($form.get(0));
+      
+  
+
+});
