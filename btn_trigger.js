2896d45d6d74faff049c6cfb8c312949a9aa523b
louisewebfx <90263713+louisewebfx@users.noreply.github.com>
2021-09-17 21:25:03 +0800

    Create btn_trigger.js

---
+++ btn_trigger.js
@@ -0,0 +1,9 @@
+$('.ant-form-item-control-input-content').on('click','.ant-btn.ant-btn-secondary',function(){ 
+  console.log('step 1');
+  var email = $('[name=email]').val();
+  var firstname = $('[name=first_name]').val();
+  var lastname = $('[name=last_name]').val();
+  $('form').append('<input type="hidden" name="email" value="' + email+ '">');
+  $('form').append('<input type="hidden" name="first_name" value="' + firstname+ '">');
+  $('form').append('<input type="hidden" name="last_name" value="' + lastname+ '">');
+});
