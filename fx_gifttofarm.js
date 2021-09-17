885d5d4a10f1562d324d655fdca5cf7709cce5e3
louisewebfx <90263713+louisewebfx@users.noreply.github.com>
2021-09-17 23:53:54 +0800

    Create fx_gifttofarm.js

---
+++ fx_gifttofarm.js
@@ -0,0 +1,9 @@
+$(document).on("click", "button.ant-btn-secondary" , function() {
+  
+     var email = $(this).closest('.step1').find('[name=email]').val();
+     var firstname = $(this).closest('.step1').find('[name=first_name]').val(); 
+     var lastname = $(this).closest('.step1').find('[name=last_name]').val(); 
+     $('.ant-form').append('<input type="hidden" name="email" value="' + email+ '">');
+     $('.ant-form').append('<input type="hidden" name="first_name" value="' + firstname+ '">');
+     $('.ant-form').append('<input type="hidden" name="last_name" value="' + lastname+ '">');
+ });
