6ec90990884a8e6dc1909c81e3378c5573ce253f
Avi Schwab <froboy@gmail.com>
2022-02-25 10:17:20 -0600

    Don't error if MCFX is blocked on the page

--- mcfx-select.js
+++ mcfx-select.js
@@ -2,7 +2,7 @@
 
     $('.webform-submission-swim-lesson-selector-form').on( 'input', e => {
         const form = e.currentTarget
-        if( 'undefined' !== MCFX ) {
+        if( "MCFX" in window ) {
             MCFX.Form.submit( form )
             console.log('form-submmited')
         }
